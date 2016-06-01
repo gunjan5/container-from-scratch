64072bd5c34a74c8619311c609d1ca8ede3fe2d0
gunjan5 <patelgunjan5@gmail.com>
2016-06-01 02:06:59 -0700

    REST APIs checkpoint1

--- main.go
+++ main.go
@@ -1,20 +1,78 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 
 	"github.com/codegangsta/cli"
+	"github.com/gorilla/mux"
 	"github.com/gunjan5/container-from-scratch/cmd"
 )
 
+const (
+	IP   = "127.0.0.1"
+	PORT = ":1337"
+)
+
+var containers = []Container{Container{"run", "TinyCore", "ls"}}
+
+type Container struct {
+	State   string
+	Image   string
+	Command string
+}
+
 func main() {
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", index)
+	router.HandleFunc("/run", getContainerHandler).Methods("GET")
+	router.HandleFunc("/run", postContainerHandler).Methods("POST")
+
+	http.Handle("/", router)
+	http.ListenAndServe(IP+PORT, nil)
 
 	app := makeCmd()
 
 	if len(os.Args) > 0 {
 		app.Run(os.Args)
 	}
+
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Welcome to Container From Scratch (CFS)!")
+}
+
+func getContainerHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	result, err := json.Marshal(containers)
+	if err != nil {
+		fmt.Errorf("Error marshaling json: %v ", err)
+	}
+	w.Write(result)
+
+}
+func postContainerHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var c Container
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Errorf("Error: %v while reading request body: %v ", r.Body)
+	}
+
+	json.Unmarshal(body, &c)
+	containers = append(containers, c)
+
+	result, err := json.Marshal(containers)
+	if err != nil {
+		fmt.Errorf("Error marshaling json: %v ", err)
+	}
+	w.Write(result)
 
 }
 
