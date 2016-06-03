cb3edd1157ee5641edf5358e3d035c9d3f74a629
gunjan5 <patelgunjan5@gmail.com>
2016-06-03 00:55:58 -0700

    basic CLI tests

--- container/cfs.go
+++ container/cfs.go
@@ -11,9 +11,9 @@ func Run(args []string) error {
 	_ = "breakpoint"
 	command := exec.Command("/proc/self/exe", append([]string{"newroot"}, args[0:]...)...)
 
-	command.SysProcAttr = &syscall.SysProcAttr{ //add some namespaces: UTS, PID, MNT
-		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
-	}
+	// command.SysProcAttr = &syscall.SysProcAttr{ //add some namespaces: UTS, PID, MNT
+	// 	Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+	// }
 	command.Stdin = os.Stdin
 	command.Stdout = os.Stdout
 	command.Stderr = os.Stderr
@@ -49,10 +49,10 @@ func NewRoot(args []string) error {
 }
 
 func Child(args []string) error {
-	check(syscall.Mount("rootfs", "rootfs", "", syscall.MS_BIND, ""))
-	check(os.MkdirAll("rootfs/oldrootfs", 0700))
-	check(syscall.PivotRoot("rootfs", "rootfs/oldrootfs"))
-	check(os.Chdir("/"))
+	// check(syscall.Mount("rootfs", "rootfs", "", syscall.MS_BIND, ""))
+	// check(os.MkdirAll("rootfs/oldrootfs", 0700))
+	// check(syscall.PivotRoot("rootfs", "rootfs/oldrootfs"))
+	// check(os.Chdir("/"))
 
 	command := exec.Command(args[0], args[1:]...)
 	command.Stdin = os.Stdin
--- main_test.go
+++ main_test.go
@@ -7,21 +7,25 @@ import (
 
 func TestMainRun(t *testing.T) {
 	cases := []struct {
-		name string
-		cmd  string
-		arg1 string
-		arg2 string
+		name  string
+		state string
+		image string
+		cmd   string
 	}{
-		{"cfs", "run", "ls", "-l"},
-	//	{"cfs", "run", "pwd", ""},
-	//	{"cfs", "invalid", "no", "-n"},
-	//	{"cfs", "run", "invalidArg", "invalidFlag"},
+		{"cfs", "run", "TinyCore", "ls"},
+		{"cfs", "run", "SlitazOS", "whoami"},
+		{"cfs", "run", "BusyBox", "pwd"},
+		//	{"cfs", "run", "pwd", ""},
+		//	{"cfs", "invalid", "no", "-n"},
+		//	{"cfs", "run", "invalidArg", "invalidFlag"},
 	}
 
 	app := makeCmd()
 
+	//TODO: figure out how to test the output for this :/
+
 	for _, entry := range cases {
-		app.Run([]string{entry.name, entry.cmd, entry.arg1, entry.arg2})
+		app.Run([]string{entry.name, entry.state, entry.image, entry.cmd})
 
 	}
 
